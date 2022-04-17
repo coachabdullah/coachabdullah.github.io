2547cc2a2319fa926718eb0c5728f5685c6c2cce
Arshad Kazmi <arshadkazmi42@gmail.com>
2022-04-17 15:40:26 +0530

    Updates

---
+++ precache-manifest.ce36f0c38f1f0474162017b691362e5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e67796cd85e10a36b5005f1e9eba514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b6b2449fda2934e7f7c",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6b2449fda2934e7f7c",
+    "url": "/static/js/main.312c075b.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
